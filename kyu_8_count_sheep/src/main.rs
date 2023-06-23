d976014daa80e24563243d3a866bb24bfdb13b8e
teewhy <tyleryeh047@gmail.com>
2023-06-23 22:36:26 +0800

    add(rust): (8 kyu) if you can't sleep, just count sheep!

---
+++ kyu_8_count_sheep/src/main.rs
@@ -0,0 +1,13 @@
+fn count_sheep(n: u32) -> String {
+    let mut str = String::new();
+    for i in 1..n+1 {
+        str += &i.to_string();
+        str += " sheep...";
+    }
+    str
+}
+
+fn main() {
+    let n = 4;
+    println!("{}", count_sheep(n));
+}
