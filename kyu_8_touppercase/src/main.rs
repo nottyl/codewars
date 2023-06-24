8b3058895f55243bfdfc31110e6ba107328fee3f
teewhy <tyleryeh047@gmail.com>
2023-06-24 14:19:19 +0800

    add(rust): (8 kyu) MakeUpperCase

---
+++ kyu_8_touppercase/src/main.rs
@@ -0,0 +1,9 @@
+// Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/rust
+
+fn make_upper_case(s: &str) -> String {
+    s.to_uppercase()
+}
+
+fn main() {
+    println!("hello ->{}", make_upper_case("hello"));
+}
