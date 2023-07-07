019eff4b1e1d033b40b97fb51eae019838be21f0
teewhy <tyleryeh047@gmail.com>
2023-07-07 13:45:07 +0800

    add(rust): (8 kyu) calculate average

---
+++ kyu_8_calculate_average/src/main.rs
@@ -0,0 +1,16 @@
+// Link: https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/rust
+
+fn find_average(slice: &[f64]) -> f64 {
+    if slice.is_empty() {
+        0.0
+    }
+    else {
+        let sum: f64 = slice.iter().sum();
+        let avg: f64 = sum / slice.len() as f64;
+        avg
+    }
+}
+
+fn main() {
+    println!("{}", find_average(&[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]));
+}
