47d75a8f74435a3aef4440be56b42ae3d6ab160a
teewhy <tyleryeh047@gmail.com>
2023-09-11 00:12:22 +0800

    new(Rust): reverse sublist

--- Rust/7 kyu/reverse_sublists/src/main.rs
+++ Rust/7 kyu/reverse_sublists/src/main.rs
@@ -1,6 +1,30 @@
 fn rev_sub(xs: &[i32]) -> Vec<i32> {
-    let mut result = xs.to_vec();
-    let items = 
+    let mut result = Vec::new();
+    let mut sub_start = 0;
+    let mut sublist = false;
+    let len = xs.len();
+
+    for i in 0..len {
+        if xs[i] % 2 == 0 {
+            if !sublist {
+                sub_start = i;
+                sublist = true;
+            }
+        } else {
+            if sublist {
+                result.extend(xs[sub_start..i].iter().rev());
+                sublist = false;
+            }
+            result.push(xs[i]);
+        }
+    }
+
+    // If the last item is also an even number
+    if sublist {
+        result.extend(xs[sub_start..].iter().rev());
+    }
+
+    result
 }
 
 fn main() {
